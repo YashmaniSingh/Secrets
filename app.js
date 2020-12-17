8b9b202f027d37bf7f04b77113c195e07edfcbf8
Yashmani Singh <yashmanisingh@Yashmanis-MacBook-Air.local>
2020-12-17 19:24:03 +0530

    3rd commit

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose =require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
-console.log(process.env.API_KEY);
+
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
@@ -35,9 +37,11 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+
   const newUser = new User({
     email: req.body.username,
-    password: md5(req.body.password)
+    password: hash
   });
   newUser.save(function(err){
     if(err){
@@ -47,22 +51,27 @@ app.post("/register",function(req,res){
     }
   });
 });
+});
 app.post("/login", function(req,res){
   const username= req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
-User.findOne({email: username}, function(err,result){
+User.findOne({email: username}, function(err,foundUser){
   if(err){
     console.log(err);
   }else{
-    if(result){
-      if(result.password === password){
-        res.render("secrets");
+    if(foundUser){
+      bcrypt.compare(password,foundUser.password,function(err,result){
+        if(result===true){
+                  res.render("secrets");
+        }
+      });
+
       }
     }
-  }
+  });
 });
-});
+
 
 app.listen(3000,function(req,res){
   console.log("Server started on port 3000");
